8d14acc5bd3887942183d5e15a2ff823da75836e
anuramineni <136206107+anuramineni@users.noreply.github.com>
2023-06-11 20:18:54 -0500

    Add files via upload

---
+++ assignments/SQLQuery5.sql
@@ -0,0 +1,6 @@
+--select*from tab_1 left join tab_2 on tab_1.table_1=tab_2.table_2;
+
+--select*from tab_1 right join tab_2 on tab_1.table_1=tab_2.table_2;
+
+--select*from tab_1 full outer join tab_2 on tab_1.table_1=tab_2.table_2;
+
